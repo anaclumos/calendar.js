5c5226a8cf98afcc67916f1a68b50839ecbe8463
Sunghyun Cho <mail@chosunghyun.com>
2022-04-08 22:20:08 -0700

    reformat calendar

--- calendar.js
+++ calendar.js
@@ -1,40 +1,39 @@
-const html = (s, ...args) => s.map((ss, i) => `${ss}${args[i] || ''}`).join('');
+const html = (s, ...args) => s.map((ss, i) => `${ss}${args[i] || ''}`).join('')
 
-const NUMBER_OF_DAYS_IN_WEEK = 7;
-const NAME_OF_DAYS = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
-
+const NUMBER_OF_DAYS_IN_WEEK = 7
+const NAME_OF_DAYS = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat']
+const LONG_NAME_OF_DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 const renderCalendar = ($target) => {
-  $target.innerHTML = getCalendarHTML();
-};
+  let html = getCalendarHTML()
+  // minify html
+  html = html.replace(/\n/g, '')
+  // replace multiple spaces with single space
+  html = html.replace(/\s{2,}/g, ' ')
+  $target.innerHTML = html
+}
 
 const processDate = (day) => {
-  const date = day.getDate();
-  const month = day.getMonth();
-  const year = day.getFullYear();
+  const date = day.getDate()
+  const month = day.getMonth()
+  const year = day.getFullYear()
   return {
     lastMonthLastDate: new Date(year, month, 0),
     thisMonthFirstDate: new Date(year, month, 1),
     thisMonthLastDate: new Date(year, month + 1, 0),
     nextMonthFirstDate: new Date(year, month + 1, 1),
-  };
-};
+  }
+}
 
 const getCalendarHTML = () => {
-  let today = new Date();
-  let {
-    lastMonthLastDate,
-    thisMonthFirstDate,
-    thisMonthLastDate,
-    nextMonthFirstDate,
-  } = processDate(today);
-  let calendarContents = [];
+  let today = new Date()
+  let { lastMonthLastDate, thisMonthFirstDate, thisMonthLastDate, nextMonthFirstDate } =
+    processDate(today)
+  let calendarContents = []
 
   for (let d = 0; d < NUMBER_OF_DAYS_IN_WEEK; d++) {
     calendarContents.push(
-      html`<div class="${NAME_OF_DAYS[d]} calendar-cell">
-        ${NAME_OF_DAYS[d]}
-      </div>`
-    );
+      html`<div class="${NAME_OF_DAYS[d]} calendar-cell">${LONG_NAME_OF_DAYS[d]}</div>`
+    )
   }
 
   for (let d = 0; d < thisMonthFirstDate.getDay(); d++) {
@@ -51,7 +50,7 @@ const getCalendarHTML = () => {
         d +
         1}
       </div>`
-    );
+    )
   }
 
   for (let d = 0; d < thisMonthLastDate.getDate(); d++) {
@@ -65,18 +64,18 @@ const getCalendarHTML = () => {
           this-month
         "
       >
-        ${today.getMonth() + 1}/${d + 1}
-        ${today.getDate() === d + 1 ? ' today' : ''}
+        ${today.getMonth() + 1}/${d + 1} ${today.getDate() === d + 1 ? ' today' : ''}
       </div>`
-    );
+    )
   }
 
-  let nextMonthDaysToRender = 7 - (calendarContents.length % 7);
+  let nextMonthDaysToRender = 7 - (calendarContents.length % 7)
 
   for (let d = 0; d < nextMonthDaysToRender; d++) {
     calendarContents.push(
       html`<div
         class="
+          ${(nextMonthFirstDate.getDay() + d) % 7 === 0 ? 'sun' : ''}
           ${(nextMonthFirstDate.getDay() + d) % 7 === 6 ? 'sat' : ''}
           calendar-cell
           next-month
@@ -84,7 +83,7 @@ const getCalendarHTML = () => {
       >
         ${nextMonthFirstDate.getMonth() + 1}/${d + 1}
       </div>`
-    );
+    )
   }
-  return calendarContents.join('');
-};
+  return calendarContents.join('')
+}
